64b709938216b08d9679fcbfd60a123c238e9430
Ashutosh <as31563156@gmail.com>
2025-07-31 12:14:16 +0530

    initial commit

---
+++ backend/src/main/java/com/example/GreenRoute/GreenRouteApplication.java
@@ -0,0 +1,13 @@
+package com.example.GreenRoute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenRouteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/GreenRoute/GreenRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GreenRoute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
